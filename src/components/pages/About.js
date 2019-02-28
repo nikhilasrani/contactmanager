6a8db116115abf69b8394bd1e61a8104b91daad7
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-02-28 17:34:27 +0530

    updated about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,12 @@ export default function About() {
   return (
     <div>
       <h1 className="display-4"> About Contact Manager</h1>
-      <p className="lead">Simple App to manage contacts</p>
-      <p className="text-secondary">Version 1.0.0</p>
+      <p className="lead">
+        A simple CRUD application built in React that uses axios to make GET,
+        DELETE, POST and PUT HTTP requests to a fake REST API.
+      </p>
+      <p className="lead">Created by Nikhil Asrani</p>
+      <p className="text-secondary">Version 1.2.0</p>
     </div>
   );
 }
